c713bf6099204bdd6ef262a0169815276ad87b37
Alhaitham Al Jabri <jabri.dev@gmail.com>
2025-05-29 00:55:40 +0400

    0155: solution

--- leetcode/0155/main.go
+++ leetcode/0155/main.go
@@ -1,48 +1,45 @@
 package main
 
-type Node struct {
-	Val int
-	Min int
-}
 type MinStack struct {
-	stack []Node
+	stack []int
+	mins  []int
 }
 
 func Constructor() MinStack {
-	return MinStack{stack: []Node{}}
+	return MinStack{
+		stack: []int{},
+		mins:  []int{},
+	}
 }
 
 func (this *MinStack) Push(val int) {
-	min := val
-	if len(this.stack) > 0 {
-		min = Min(val, this.stack[len(this.stack)-1].Min)
+	this.stack = append(this.stack, val)
+	if len(this.mins) == 0 || val <= this.mins[len(this.mins)-1] {
+		this.mins = append(this.mins, val)
 	}
-	this.stack = append(this.stack, Node{Val: val, Min: min})
 }
 
 func (this *MinStack) Pop() {
-	if len(this.stack) > 0 {
-		this.stack = this.stack[:len(this.stack)-1]
+	if len(this.stack) == 0 {
+		return
+	}
+	pop := this.stack[len(this.stack)-1]
+	this.stack = this.stack[:len(this.stack)-1]
+	if len(this.mins) > 0 && pop == this.mins[len(this.mins)-1] {
+		this.mins = this.mins[:len(this.mins)-1]
 	}
 }
 
 func (this *MinStack) Top() int {
-	if len(this.stack) > 0 {
-		return this.stack[len(this.stack)-1].Val
+	if len(this.stack) == 0 {
+		return 0
 	}
-	return -1
+	return this.stack[len(this.stack)-1]
 }
 
 func (this *MinStack) GetMin() int {
-	if len(this.stack) > 0 {
-		return this.stack[len(this.stack)-1].Min
+	if len(this.mins) == 0 {
+		return 0
 	}
-	return -1
-}
-
-func Min(x, y int) int {
-	if x < y {
-		return x
-	}
-	return y
+	return this.mins[len(this.mins)-1]
 }
